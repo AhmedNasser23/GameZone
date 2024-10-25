840b9cc775076ba6520fb999e5b0ad327896f2d6
Ahmed Nasser <ahmednasser233005@gmail.com>
2024-10-25 23:10:24 +0300

    Add project files.

---
+++ GameZone/Data/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace GameZone.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
